3d38f400313e0671cd4b6fdb74c2fc10cd20534e
dagabate <dagtfsm@gmail.com>
2025-06-01 18:32:33 -0400

    Add to Cart Funcationality Prepared

--- amazon-clone-2025/src/Components/Header/Header.jsx
+++ amazon-clone-2025/src/Components/Header/Header.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import classes from "./Header.module.css";
 import { SlLocationPin } from "react-icons/sl";
 import { BsSearch } from "react-icons/bs";
 import { BiCart } from "react-icons/bi";
 import { Link } from "react-router-dom";
+import { DataContext } from "../DataProvider/DataProvider";
+
 function Header() {
+  const [{ basket }, dispatch] = useContext(DataContext);
   return (
     <section>
       <section>
@@ -61,7 +64,7 @@ function Header() {
             {/* cart */}
             <Link to="/cart" className={classes.cart}>
               <BiCart size={35} />
-              <span>0</span>
+              <span>{basket.length}</span>
             </Link>
           </div>
         </div>
--- amazon-clone-2025/src/Components/Product/ProductCard.jsx
+++ amazon-clone-2025/src/Components/Product/ProductCard.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import Rating from "@mui/material/Rating";
 import CurrencyFormat from "../CurrencyFormat/CurrencyFormat";
 import classes from "./Product.module.css";
 import { Link } from "react-router-dom";
+import { DataContext } from "../DataProvider/DataProvider";
+import { Type } from "../../Utility/action.type";
 function ProductCard({ product, flex, renderDesc }) {
   const { title, image, price, rating, id, description } = product;
+  const [state, dispatch] = useContext(DataContext);
+  const addToCart = () => {
+    dispatch({
+      type: Type.ADD_TO_BASKET,
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+        description,
+      },
+    });
+  };
   return (
     <div
       className={`${classes.card_container} ${
@@ -30,7 +46,7 @@ function ProductCard({ product, flex, renderDesc }) {
           <div className="">
             <CurrencyFormat amount={price} />
           </div>
-          <button className={classes.button}>
+          <button className={classes.button} onClick={addToCart}>
             <span>Add to Cart</span>
           </button>
         </div>
