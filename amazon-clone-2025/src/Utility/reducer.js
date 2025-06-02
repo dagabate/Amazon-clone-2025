c556bcac89d5d9817b05c5923d3b6a6fca7bd4a0
dagabate <dagtfsm@gmail.com>
2025-06-02 00:11:00 -0400

    Cart items not repeated

--- amazon-clone-2025/src/Utility/reducer.js
+++ amazon-clone-2025/src/Utility/reducer.js
@@ -5,10 +5,26 @@ export const initialState = {
 export const reducer = (state, action) => {
   switch (action.type) {
     case Type.ADD_TO_BASKET:
-      return {
-        ...state,
-        basket: [...state.basket, action.item],
-      };
+      const existingItem = state.basket.find(
+        (item) => item.id === action.item.id
+      );
+      if (!existingItem) {
+        // If the item already exists, we can choose to update it or ignore the action
+        return {
+          ...state,
+          basket: [...state.basket, { ...action.item, amount: 1 }],
+        }; // or you could update the quantity or other properties
+      } else {
+        const updatedBasket = state.basket.map((item) =>
+          item.id === action.item.id
+            ? { ...item, amount: item.amount + 1 } // Increment the amount if it exists
+            : item
+        );
+        return {
+          ...state,
+          basket: updatedBasket,
+        };
+      }
     default:
       return state;
   }
